cd2404201a615a948eb3ceb156f6683818b03a35
Nathanael Bocker <nathanael.j.bocker@gmail.com>
2026-06-19 11:16:21 -0400

    Reduce repo to validator submission files

--- run_mountain_warehouse_diagnostics.py
+++
@@ -1,6 +0,0 @@
-from test_real_env import run_real_env_trial
-
-for terrain_id, name in [(3, "Mountain"), (5, "Warehouse")]:
-    print("=" * 60)
-    print(name)
-    print(run_real_env_trial(terrain_id, 1337, max_steps=3000, verbose=True))
